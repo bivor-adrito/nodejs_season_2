bc061f0a614b5acdaa982ea1c1ea7dddc5261fd5
bivor-adrito <bivor500@gmail.com>
2024-01-07 22:43:59 +0600

    User schema and users route created

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose')
-const uri = process.env.MONGODB_URI
+const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI;
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(
-            uri
-        )
-        console.log('MongoDB Connected')
-        
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -10,62 +10,65 @@ app.use(bodyParser.json())
 //connect with mongoDB
 connectDB()
 
+// routes
+app.use('/api/users', require('./routes/api/users'))
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({message: 'Welcome to my application.'})
 })
 
-let users = []
-let lastId = 0
+// let users = []
+// let lastId = 0
 
 // create a user
-app.post('/users', (req, res) => {
-    const user = req.body
-    user.id = ++lastId
-    users.push(user)
-    res.json(user)
-})
+// app.post('/users', (req, res) => {
+//     const user = req.body
+//     user.id = ++lastId
+//     users.push(user)
+//     res.json(user)
+// })
 
 // retrieve all users
-app.get('/users', (req, res) => {
-    res.json(users)
-})
+// app.get('/users', (req, res) => {
+//     res.json(users)
+// })
 // retrieve one users
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id
-    const user = users.find((u) => u.id == id)
-    if(user) {
-        res.json(user)
-    }else{
-        res.status(404).json({message: "User not found."})
-    }
-})
+// app.get('/users/:id', (req, res) => {
+//     const id = req.params.id
+//     const user = users.find((u) => u.id == id)
+//     if(user) {
+//         res.json(user)
+//     }else{
+//         res.status(404).json({message: "User not found."})
+//     }
+// })
 
 // update a user by specific id
-app.put('/users/:id',(req, res) => {
-    const id = parseInt(req.params.id)
-    const userUpdate = req.body
-    const userIndex = users.findIndex((u)=>u.id == id)
-    if (userIndex !== -1){
-        let user = users[userIndex]
-        user = { ...user, ...userUpdate}
-        res.json(user)
-    }else{
-        res.status(404).json({message: "User not found."})
-    }
-})
+// app.put('/users/:id',(req, res) => {
+//     const id = parseInt(req.params.id)
+//     const userUpdate = req.body
+//     const userIndex = users.findIndex((u)=>u.id == id)
+//     if (userIndex !== -1){
+//         let user = users[userIndex]
+//         user = { ...user, ...userUpdate}
+//         res.json(user)
+//     }else{
+//         res.status(404).json({message: "User not found."})
+//     }
+// })
 
 // delete a specific user
-app.delete('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const userIndex = users.findIndex((u) => u.id === id)
-    if(userIndex !== -1){
-        users.splice(userIndex, 1)
-        res.json({message: "User is deleted."})
-    }else{
-        res.status(404).json({message: "User not found."})
-    }
-})
+// app.delete('/users/:id', (req, res) => {
+//     const id = parseInt(req.params.id)
+//     const userIndex = users.findIndex((u) => u.id === id)
+//     if(userIndex !== -1){
+//         users.splice(userIndex, 1)
+//         res.json({message: "User is deleted."})
+//     }else{
+//         res.status(404).json({message: "User not found."})
+//     }
+// })
 
 
 //* Start the Server
